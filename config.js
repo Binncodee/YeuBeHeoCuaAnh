9f302dcdc2abdb58d02fbddd895f2c2cfd0bc0f4
Trần Hoàng Hải <92655443+Binncodee@users.noreply.github.com>
2023-03-08 05:12:11 +0700

    Yeu em
    Love You

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+  introTitle: "Bé Yêuuu Ơii! 🤤",
+  introDesc: `Chẳng là nay anh có điều muốn hỏi em về chuyện của chúng mình. Dù thế nào cũng phải trả lời cho thật lòng đấy nhá 😘`,
+  btnIntro: "Dạaaaa ❤",
+  title: "Em yêu anh có phải không 😘😘",
+  desc: "Với thế giới, em chỉ là một người. Nhưng, với anh, Em là cả một thế giới bao la rộng lớn ấy 😍",
+  btnYes: "Yêu lắm luôn 😛",
+  btnNo: "Ghét ơi là ghét 😑",
+  question: "E có thể nói cho anh nghe lý do vì sao em yêu anh đi nào <3",
+  btnReply: "Gửi cho anh yêu nà <3",
+  reply:
+    "Tại vì là anh đẹp zai, dễ thương và đặc biệt là yêu em rất nhìu <3. hihi",
+  mess: "Hehe anh biết mà, cảm ơn em đã đến bên anh ngay tại giây phút này. Anh chỉ muốn nói rằng là: Anh ❤ Em💖. Em làm người yêu của anh nha?💕 ",
+  messDesc: "Hihi 🤣🤣🤣",
+  btnAccept:
+    "Zạaaaaa anh <3, có gì ib anh liền nhá😜, anh đợi e câu trả lời đó😘",
+  messLink: "https://www.facebook.com/messages/t/100013291230771",
+};
